58cd6d99980202559e17280a6881555767a9ce75
Nati Namvong <nati@flyingcomma.com>
2018-01-09 19:45:06 +0700

    Add UI

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-4 is-offset-4">
+            <b-field>
+              <b-input placeholder="What needs to be done?"
+                type="text"
+                icon="check"
+                size="is-large">
+              </b-input>
+            </b-field>
+
+            <br>
+
+            <b-field>
+              <b-checkbox size="is-large">Large</b-checkbox>
+            </b-field>
+
+            <b-field>
+              <b-checkbox size="is-large">Large</b-checkbox>
+            </b-field>
+
+            <hr>
+
+            <p class="is-pulled-left">3 items left</p>
+            
+            <b-field class="is-pulled-right">
+              <b-radio-button v-model="radioButton"
+                native-value="Nop"
+                type="is-success">
+                <span>All</span>
+              </b-radio-button>
+
+              <b-radio-button v-model="radioButton"
+                native-value="Yep"
+                type="is-success">
+                <span>Active</span>
+              </b-radio-button>
+
+              <b-radio-button v-model="radioButton"
+                native-value="Yep"
+                type="is-success">
+                <span>Completed</span>
+              </b-radio-button>
+            </b-field>
+
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
